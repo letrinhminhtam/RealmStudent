dc5972007dba6ae82843f1666a4a0ec544c133c7
TamLTM <Tamltm@asiantech.vn>
2016-07-19 09:18:26 +0700

    Create Project

---
+++ Student/Define/Strings.swift
@@ -0,0 +1,13 @@
+//
+//  Strings.swift
+//  Student
+//
+//  Created by TamLTM on 7/19/16.
+//  Copyright © 2016 Asiantech. All rights reserved.
+//
+
+import UIKit
+
+class Strings {
+    let home = "Home"
+}
---
+++ Student/HomeViewController/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  Student
+//
+//  Created by TamLTM on 7/19/16.
+//  Copyright © 2016 Asiantech. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
